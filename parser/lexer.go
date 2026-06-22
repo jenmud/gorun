bf4be02efb25964ddb3c04834da6c84f56492002
Jenda Mudron <jenmud@gmail.com>
2026-06-22 21:07:15 +1000

    some more updates. Dropped the Next for now

--- parser/lexer.go
+++ parser/lexer.go
@@ -2,43 +2,28 @@ package parser
 
 // Lexer which is used for parsing the input into tokens
 type Lexer struct {
-	input   []byte
+	input   string
 	line    int // current line of the file
 	curPos  int // cursor position
 	readPos int // read position - look ahead
 }
 
 // NewLexer returns a new lexer for the providing input string.
-func NewLexer(input []byte) *Lexer {
-	l := &Lexer{
+func NewLexer(input string) *Lexer {
+	return &Lexer{
 		input: input,
 	}
-
-	if len(input) > 0 {
-		l.readPos++
-	}
-
-	return l
 }
 
-// Next returns true if there are more tokens to process
-// and advances the cursor till end of file is reached.
-func (l *Lexer) Next() bool {
-	if len(l.input) == 0 {
-		// nothing to do
-		return false
+// readChar will return the current charactor and advance the cursor.
+// Rune `0` indicates EOF.
+func (l *Lexer) readChar() rune {
+	if l.readPos >= len(l.input) {
+		return 0
 	}
 
-	if l.readPos > len(l.input) {
-		return false
-	}
-
-	l.curPos++
+	ch := l.input[l.readPos]
+	l.curPos = l.readPos
 	l.readPos++
-	return true
-}
-
-// Token returns the current token at the cursor position.
-func (l *Lexer) Token() Token {
-	return Token{}
+	return rune(ch)
 }
--- parser/lexer_test.go
+++ parser/lexer_test.go
@@ -11,81 +11,41 @@ type LexerTestSuite struct {
 	lexer *Lexer
 }
 
-func (suite *LexerTestSuite) TestLexing__Next__empty_string() {
-	input := []byte{}
+func (suite *LexerTestSuite) TestLexing__readChar() {
+	input := "ab"
 
 	l := NewLexer(input)
 	suite.NotNil(l)
 
-	suite.False(l.Next())
-	suite.Equal(0, l.curPos)
-	suite.Equal(0, l.readPos)
+	suite.Equal('a', l.readChar())
+	suite.Equal('b', l.readChar())
+	suite.Equal(rune(0), l.readChar())
 }
 
-func (suite *LexerTestSuite) TestLexing__Next__longer_example() {
-	input := []byte(`
-		hostname:
-			echo "$HOST"
-	`)
-
-	l := NewLexer(input)
-	suite.NotNil(l)
-
-	// test starts at cursor position 1 and read position 2
-	// because when you call .Next() it has alrady advanced
-	c := 1
-	r := 2
-	for l.Next() {
-		suite.Equal(c, l.curPos)
-		suite.Equal(r, l.readPos)
-		c++
-		r++
-	}
-}
-
-func (suite *LexerTestSuite) TestLexing__Next() {
-	input := []byte(`ab`)
-
-	l := NewLexer(input)
-	suite.NotNil(l)
-
-	suite.True(l.Next())
-	suite.Equal(1, l.curPos)
-	suite.Equal(2, l.readPos)
-
-	suite.True(l.Next())
-	suite.Equal(2, l.curPos)
-	suite.Equal(3, l.readPos)
-
-	suite.False(l.Next())
-	suite.Equal(2, l.curPos)
-	suite.Equal(3, l.readPos)
-}
-
-func (suite *LexerTestSuite) _TestLexing__Single_task() {
-	input := []byte(`
-	hostname:
-		echo $HOST
-	`)
-
-	lexer := NewLexer(input)
-	suite.NotNil(lexer)
-
-	want := []Token{
-		Token{Type: IDENTIFIER, Value: []byte("hostname")},
-		Token{Type: COLON, Value: []byte(":")},
-		Token{Type: CMD, Value: []byte("echo $HOST")},
-		Token{Type: EOF, Value: nil},
-	}
-
-	got := []Token{}
-
-	for suite.lexer.Next() {
-		got = append(got, suite.lexer.Token())
-	}
-
-	suite.ElementsMatch(want, got)
-}
+//func (suite *LexerTestSuite) _TestLexing__Single_task() {
+//	input := []byte(`
+//	hostname:
+//		echo $HOST
+//	`)
+//
+//	lexer := NewLexer(input)
+//	suite.NotNil(lexer)
+//
+//	want := []Token{
+//		Token{Type: IDENTIFIER, Value: []byte("hostname")},
+//		Token{Type: COLON, Value: []byte(":")},
+//		Token{Type: CMD, Value: []byte("echo $HOST")},
+//		Token{Type: EOF, Value: nil},
+//	}
+//
+//	got := []Token{}
+//
+//	for suite.lexer.Next() {
+//		got = append(got, suite.lexer.Token())
+//	}
+//
+//	suite.ElementsMatch(want, got)
+//}
 
 func TestLexingTestSuite(t *testing.T) {
 	suite.Run(t, new(LexerTestSuite))
